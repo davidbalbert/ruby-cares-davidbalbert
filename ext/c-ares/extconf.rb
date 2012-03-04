dff466ed72c45292bebf8061ff49bfe4a46f43ad
Steve von Takach <steve@advancedcontrol.com.au>
2012-03-04 21:53:50 +1100

    added greater platform support

--- ext/c-ares/extconf.rb
+++ ext/c-ares/extconf.rb
@@ -1,7 +1,22 @@
 require 'mkmf'
 
-uname = `uname -s`.chomp.downcase
-
-find_header("ares_config.h", File.expand_path("../config_#{uname}", __FILE__))
+case RUBY_PLATFORM
+	when /mswin32/, /mingw32/, /bccwin32/
+		find_header("ares_config.h", File.expand_path("../config_win32", __FILE__))
+	when /solaris/
+		find_header("ares_config.h", File.expand_path("../config_sunos", __FILE__))
+	when /openbsd/
+		find_header("ares_config.h", File.expand_path("../config_openbsd", __FILE__))
+	when /darwin/
+		find_header("ares_config.h", File.expand_path("../config_darwin", __FILE__))
+	when /linux/
+		find_header("ares_config.h", File.expand_path("../config_linux", __FILE__))
+	when /cygwin/
+		find_header("ares_config.h", File.expand_path("../config_cygwin", __FILE__))
+	when /netbsd/
+		find_header("ares_config.h", File.expand_path("../config_netbsd", __FILE__))
+	when /freebsd/
+		find_header("ares_config.h", File.expand_path("../config_freebsd", __FILE__))
+end
 $defs.push "-DHAVE_CONFIG_H"
 create_makefile('cares/cares')
