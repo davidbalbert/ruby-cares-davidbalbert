b5ab2f4793c54e10673cc3f00c925c8e4e8338e9
Steve von Takach <steve@advancedcontrol.com.au>
2012-03-05 00:44:36 +1100

    slightly more cross platform

--- ext/c-ares/extconf.rb
+++ ext/c-ares/extconf.rb
@@ -2,21 +2,24 @@ require 'mkmf'
 
 case RUBY_PLATFORM
 	when /mswin32/, /mingw32/, /bccwin32/
-		have_header("config_win32/ares_config.h")
+		find_header('ares_config.h','./config_win32')
 	when /solaris/
-		have_header("config_sunos/ares_config.h")
+		find_header('ares_config.h','./config_sunos')
 	when /openbsd/
-		have_header("config_openbsd/ares_config.h")
+		find_header('ares_config.h','./config_openbsd')
 	when /darwin/
-		have_header("config_darwin/ares_config.h")
+		find_header('ares_config.h','./config_darwin')
 	when /linux/
-		have_header("config_linux/ares_config.h")
+		find_header('ares_config.h','./config_linux')
 	when /cygwin/
-		have_header("config_cygwin/ares_config.h")
+		find_header('ares_config.h','./config_cygwin')
 	when /netbsd/
-		have_header("config_netbsd/ares_config.h")
+		find_header('ares_config.h','./config_netbsd')
 	when /freebsd/
-		have_header("config_freebsd/ares_config.h")
+		find_header('ares_config.h','./config_freebsd')
 end
+
+$CFLAGS << " -DHAVE_CONFIG_H"
+$defs.push "-DHAVE_CONFIG_H"
 
 create_makefile('cares/cares')
